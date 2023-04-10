7b70d9fb4ce17134cedfedba18c7453697fc8526
Rohit <rohit.kumar@bigsteptech.com>
2023-04-10 14:01:48 +0530

    Added GIF Supports with dynamic Configurations.

--- Package.swift
+++ Package.swift
@@ -18,18 +18,28 @@ let package = Package(
             name: "JioMeetRtcEngine_iOS",
             url: "https://github.com/JioMeet/JioMeet-rtc-engine-ios.git",
             "3.7.2"..<"3.7.3"
+        ),
+        .package(
+            name: "SDWebImage",
+            url: "https://github.com/SDWebImage/SDWebImage.git",
+            from: "5.10.0"
+        ),
+        .package(
+            name: "SDWebImageSwiftUI",
+            url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git",
+            from: "2.0.0"
         )
     ],
     targets: [
         .binaryTarget(
             name: "JioMeetCoreSDK",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_4/JioMeetCoreSDK.xcframework.zip",
-            checksum: "c304728b0a024ff33beecf32a7ba2a7cd7db1bbbffbfeccecd685cc9e1ba3a8d"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_5/JioMeetCoreSDK.xcframework.zip",
+            checksum: "01776ef6b907562bede1842939f5a95cab6351afc5fbec35e88766f321d2b17c"
         ),
         .binaryTarget(
             name: "JioMeetWatchParty",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_4/JioMeetWatchParty.xcframework.zip",
-            checksum: "a292b1a83511dd16f848be859a30865537c7e863b9c432e05353347d7dc055e8"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_5/JioMeetWatchParty.xcframework.zip",
+            checksum: "2ec80a6014796c47dc2654f369f72348d061e7228d312acdcafb137f16ecad2a"
         ),
         .target(
             name: "JioMeetWatchPartyTarget",
@@ -37,7 +47,9 @@ let package = Package(
                 .target(name: "JioMeetCoreSDK"),
                 .target(name: "JioMeetWatchParty"),
                 .product(name: "RTC", package: "JioMeetRtcEngine_iOS"),
-                .product(name: "RTM", package: "JioMeetRtcEngine_iOS")
+                .product(name: "RTM", package: "JioMeetRtcEngine_iOS"),
+                .product(name: "SDWebImageSwiftUI", package: "SDWebImageSwiftUI"),
+                .product(name: "SDWebImage", package: "SDWebImage")
             ],
             path: "Framework/Dependency",
             exclude: []
