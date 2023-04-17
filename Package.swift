75ff4bd3f58dd10dc71415c272253b27d35aae97
Rohit <rohit.kumar@bigsteptech.com>
2023-04-17 17:04:25 +0530

    Resolved Keyboard Issue and Rejoing case

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JioMeetCoreSDK",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_7/JioMeetCoreSDK.xcframework.zip",
-            checksum: "3010114ef0abd323b0a370b0d9ad36d97f1c205e27368866a3f8ff5676bbab50"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_8/JioMeetCoreSDK.xcframework.zip",
+            checksum: "eb418392299c0e6b0cc945761d9189ba46cafa91636595fcf8f2088a4bc0caf9"
         ),
         .binaryTarget(
             name: "JioMeetWatchParty",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_7/JioMeetWatchParty.xcframework.zip",
-            checksum: "b5e443828c4c01416418e3ae9c5253c7bef9ba071b4dce7b28df4d59532c01d3"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_8/JioMeetWatchParty.xcframework.zip",
+            checksum: "5937cd45718f7bb561cfe23bd2666f64bb5b358a8a31b210abf5c523c73507ef"
         ),
         .binaryTarget(
             name: "FLAnimatedImage",
