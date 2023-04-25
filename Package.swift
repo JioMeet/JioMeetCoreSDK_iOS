c5c630c8270f42b887d4738fb86f66287658ddd2
Rohit <rohit.kumar@bigsteptech.com>
2023-04-25 17:30:12 +0530

    Resolved Media Player Volume

--- Package.swift
+++ Package.swift
@@ -23,18 +23,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JioMeetCoreSDK",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_10/JioMeetCoreSDK.xcframework.zip",
-            checksum: "7b6196452b93b80846178ad8d642a63e326ff3f1522e24b08b7497ea5d971515"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_11/JioMeetCoreSDK.xcframework.zip",
+            checksum: "2c69dcdd8aaf4578b87bdc93a94c5f6cbbc54c2fe727ba595bf5dd04e7b2f977"
         ),
         .binaryTarget(
             name: "JioMeetWatchParty",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_10/JioMeetWatchParty.xcframework.zip",
-            checksum: "bd79392052fd876209de7547476865194678f00c590dc215b4d5ba3276ac6612"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_11/JioMeetWatchParty.xcframework.zip",
+            checksum: "e0156a3d07737027fa4e9bb29a5120d0f64478bbe6f156fe6b7cbe80040f7788"
         ),
         .binaryTarget(
             name: "FLAnimatedImage",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_10/FLAnimatedImage.xcframework.zip",
-            checksum: "9c25b81d85a55d8b71c4da992958a22ff57339e909de5b926e40af6188222026"
+            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v1_1_11/FLAnimatedImage.xcframework.zip",
+            checksum: "8fc6f7a03ed70a7e4d28b8b5f920cb4ecf189fd8c852138b1c243066716084da"
         ),
         .target(
             name: "JioMeetWatchPartyTarget",
