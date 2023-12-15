f06c68723c92217e1bd15c7cc3bd870096143075
Rohit <rohit.kumar@bigsteptech.com>
2023-12-15 16:04:15 +0530

    Updated Package.swift files

--- Package.swift
+++ Package.swift
@@ -4,43 +4,43 @@
 import PackageDescription
 
 let package = Package(
-    name: "JioMeetCoreSDK",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v12)],
-    products: [
-        .library(
-            name: "JioMeetCoreSDK",
-            targets: ["JioMeetCoreSDKTarget"]
-        ),
-    ],
-    dependencies: [
-        .package(
+	name: "JioMeetCoreSDK",
+	defaultLocalization: "en",
+	platforms: [.iOS(.v12)],
+	products: [
+		.library(
+			name: "JioMeetCoreSDK",
+			targets: ["JioMeetCoreSDKTarget"]
+		),
+	],
+	dependencies: [
+		.package(
 			name: "JioMeetRtcEngine_iOS",
 			url: "https://github.com/JioMeet/JioMeet-rtc-engine-ios.git",
 			.exact("3.7.2")
 		),
-        .package(
-            name: "JioMeetMediaStackSDK",
-            url: "https://github.com/JioMeet/JMMediaStackSDK_iOS.git", 
-           .upToNextMajor(from: "1.0.0")
-        )
-    ],
-    targets: [
-        .binaryTarget(
-            name: "JioMeetCoreSDK",
-            url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/public/new_arch/spm/v_3_0_0_alpha_4/JioMeetCoreSDK.xcframework.zip",
-            checksum: "0532559fa16c488dd2055e3c86cb775fb6efca65a1b15dce819c7d2a0544887b"
-        ),
-        .target(
-            name: "JioMeetCoreSDKTarget",
-            dependencies: [
-                .target(name: "JioMeetCoreSDK"),
-                .product(name: "RTC", package: "JioMeetRtcEngine_iOS"),
+		.package(
+			name: "JioMeetMediaStackSDK",
+			url: "https://github.com/JioMeet/JMMediaStackSDK_iOS.git",
+				.upToNextMajor(from: "1.0.0")
+		)
+	],
+	targets: [
+		.binaryTarget(
+			name: "JioMeetCoreSDK",
+			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/public/new_arch/spm/v_3_0_0_alpha_4/JioMeetCoreSDK.xcframework.zip",
+			checksum: "0532559fa16c488dd2055e3c86cb775fb6efca65a1b15dce819c7d2a0544887b"
+		),
+		.target(
+			name: "JioMeetCoreSDKTarget",
+			dependencies: [
+				.target(name: "JioMeetCoreSDK"),
+				.product(name: "RTC", package: "JioMeetRtcEngine_iOS"),
 				.product(name: "RTM", package: "JioMeetRtcEngine_iOS"),
-                .product(name: "JioMeetMediaStackSDK", package: "JioMeetMediaStackSDK")
-            ],
-            path: "Framework/Dependency",
-            exclude: []
-        ),
-    ]
+				.product(name: "JioMeetMediaStackSDK", package: "JioMeetMediaStackSDK")
+			],
+			path: "Framework/Dependency",
+			exclude: []
+		),
+	]
 )
