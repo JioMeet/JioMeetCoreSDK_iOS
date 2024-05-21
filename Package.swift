98d0674424962c52f0586016f7d257c7e22a3e34
rohit41-kumar <115720489+rohit41-kumar@users.noreply.github.com>
2024-05-21 12:01:09 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
 		.package(
 			name: "JioMeetMediaStackSDK",
 			url: "https://github.com/JioMeet/JMMediaStackSDK_iOS.git",
-			.upToNextMajor(from: "1.0.0")
+			.upToNextMajor(from: "2.0.0-alpha.1")
 		)
 	],
 	targets: [
