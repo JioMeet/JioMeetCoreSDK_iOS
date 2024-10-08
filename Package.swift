a4250e039fc6235bda68ad665ef04d450986b969
Atinder1989 <33986429+Atinder1989@users.noreply.github.com>
2024-10-08 10:07:42 +0530

    Update Version - 4.0.3

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "JioMeetCoreSDK",
-			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/public/new_arch/4_0_2/JioMeetCoreSDK.xcframework.zip",
-			checksum: "886e28d6155169f7c733fbfcbb48cfa5da58e144c572b65f12ee74c15fe29eca"
+			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/public/new_arch/4_0_3/JioMeetCoreSDK.xcframework.zip",
+			checksum: "468e71d16e0b3c006b558306c2bf92c215f020f0df139b19d3fc9f7ebc541fff"
 		),
 		.target(
 			name: "JioMeetCoreSDKTarget",
